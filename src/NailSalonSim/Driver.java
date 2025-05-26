e05bb76a72bef30d10e584f73efbd0a514f83b4e
Lynnece Eutsey <lynxe-dev@outlook.com>
2025-05-26 00:11:19 -0500

    refactor Driver file

--- src/NailSalonSim/Driver.java
+++ src/NailSalonSim/Driver.java
@@ -10,28 +10,29 @@ public class Driver {
 
         // start the day
 
-        // create new service objects
-        Service manicure = new Service("Manicure", 30);
-        Service gelMani = new Service("Gel Manicure", 40);
-        Service pedicure = new Service("Pedicure", 35);
-        Service gelPedi = new Service("Gel Pedicure", 55);
-
-        // create new workday objects
-        WorkDay workday = new WorkDay();
-
-        // workday object gets the list to hold services (WorkDay class) and adds service objects above
-        workday.getServices().add(manicure);
-        workday.getServices().add(gelMani);
-        workday.getServices().add(pedicure);
-        workday.getServices().add(gelPedi);
-
-        workday.calculateTotals();
-        workday.countCustomers();
         String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
         for (String day : daysOfWeek) {
+
             if (day.equals("Monday")) {
-                System.out.println( day + " | You're off on this day");
+                System.out.println( "Day: " + day + " | You're off on this day");
             } else {
+                // create new service objects
+                Service manicure = new Service("Manicure", 30);
+                Service gelMani = new Service("Gel Manicure", 40);
+                Service pedicure = new Service("Pedicure", 35);
+                Service gelPedi = new Service("Gel Pedicure", 55);
+
+                // create new workday objects
+                WorkDay workday = new WorkDay();
+
+                // workday object gets the list to hold services (WorkDay class) and adds service objects above
+                workday.getServices().add(manicure);
+                workday.getServices().add(gelMani);
+                workday.getServices().add(pedicure);
+                workday.getServices().add(gelPedi);
+
+                workday.calculateTotals();
+                workday.countCustomers();
                 workday.calculateTotals();
                 workday.countCustomers();
                 System.out.println("Day: " + day +
