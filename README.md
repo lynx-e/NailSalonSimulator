c7a1948f580e28eeca6bbf64ce1ec5bf5dff561e
Lynnece Eutsey <lynxe-dev@outlook.com>
2025-05-25 21:05:34 -0500

    rewrite readme file and create Service class

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## DESCRIPTION
 
-This is a project to simulate a typical day in a nail salon. 
-I work as a nail tech and I'm using this simulator to learn and practice different kinds of loops.
+This project simulates a typical day in a nail salon.
+
+As a real-life nail tech, I'm using this simulator to practice different types of loops in Java while modeling the structure of my actual workdays.
 
 ## USER STORY
 
-As a user, I'd like to simulate a typical day in a nail salon. I'd like to check if today is a working day,
-work within my shift, keep track of the number of customers I see in a day as well as my total earnings for the day. 
+As a user, I want to simulate a typical day in a nail salon.  
+I want to check if today is a working day, operate within a shift window, track the number of customers I serve, and calculate my total earnings for the day.
 
 ## UPDATES COMING SOON
--- src/NailSalonSim/Service.java
+++ src/NailSalonSim/Service.java
@@ -1,4 +1,74 @@
+/**
+ * This class holds the service class with params (name & service), I've provided getters & setters
+ * as well as an actionable void method called performService()
+ */
+
 package NailSalonSim;
 
 public class Service {
+
+    // PRIVATE VARIABLES
+    private String name;
+    private int price;
+
+
+    // DEFAULT CONSTUCTOR
+    /**
+     * Construct a new {@code Service} with default values
+     * <ul>
+     *     <li>Name: "No name"</li>
+     *     <li>Service: 0</li>
+     * </ul>
+     */
+    public Service() {
+        this.name = "No name";
+        this.price = 0;
+    }
+    // PARAMETERIZED CONTRUCTOR
+    /**
+     * Construct a new {@code Service} with specified values
+     */
+    public Service(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // performService method
+    public void performService() {
+        System.out.println("Performing " + name + " for $" + price);
+    }
+
+    // GETTERS & SETTERS
+
+    /**
+     * Get name
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set name
+     * @param name the name of the service
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get price
+     * @return price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * Set price
+     * @param price the price of the service
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
